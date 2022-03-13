49c4117949019e327ffbf2b1060eac64e8f14cba
Dhruv <samantdhruv@gmail.com>
2022-03-13 16:28:31 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
-import "./App.css";
 import { Footer, Sidebar } from "./Components";
 
 function App() {

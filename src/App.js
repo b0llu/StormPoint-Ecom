0e70948ad64a9e1c4eea8985b4346743bf620c50
Dhruv <samantdhruv@gmail.com>
2022-03-23 09:29:03 +0530

    spacing

--- src/App.js
+++ src/App.js
@@ -39,12 +39,13 @@ function App() {
               </ProductContainer>
             }
           />
-
+          
           <Route element={<RequireAuth />}>
             <Route path="/cart" element={<CartPage />} />
             <Route path="/wishlist" element={<WishlistPage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
           </Route>
+          
           <Route element={<RestrictAuth />}>
             <Route
               path="/login"

3ca55b7b297cebc4d1cba04e8bf560e7be66759b
Dhruv <samantdhruv@gmail.com>
2022-06-22 09:07:13 +0530

    style : responsiveness

--- src/Pages/ProductPage/ProductComponents/SectionContainer/SectionContainer.jsx
+++ src/Pages/ProductPage/ProductComponents/SectionContainer/SectionContainer.jsx
@@ -1,11 +1,13 @@
 import { Pagination } from "../../../../Components";
 import { useFilterReducerContext } from "../../../../context/FilterReducer.context";
 import { usePaginationContext } from "../../../../context/Pagination.context";
+import { useFilterFunctionCombiner } from "../../../../Hook/useFilterFunctionCombiner";
 import "./SectionContainer.css";
 
 export const SectionContainer = ({ children }) => {
-  const { dispatch, products } = useFilterReducerContext();
+  const { dispatch } = useFilterReducerContext();
   const { pageProducts, paginate } = usePaginationContext();
+  const { sortedProducts } = useFilterFunctionCombiner();
 
   return (
     <section>
@@ -17,7 +19,7 @@ export const SectionContainer = ({ children }) => {
         <div className="children-container">{children}</div>
         <Pagination
           postsPerPage={pageProducts.postsPerPage}
-          totalPost={products.length}
+          totalPost={sortedProducts.length}
           paginate={paginate}
           currentPage={pageProducts.currentPage}
         />
